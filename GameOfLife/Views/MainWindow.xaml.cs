4fa7916ed4f6d7cd59c78fd759cd7a11787fee4a
Alex Titarenko <alx.titarenko@gmail.com>
2019-12-22 21:27:59 -0800

    fix bug with file extensions

--- GameOfLife/Views/MainWindow.xaml.cs
+++ GameOfLife/Views/MainWindow.xaml.cs
@@ -445,11 +445,15 @@ namespace TAlex.GameOfLife.Views
             Initialize();
             
             string[] args = Environment.GetCommandLineArgs();
-            
-            var activationData = AppDomain.CurrentDomain.SetupInformation.ActivationArguments.ActivationData;
-            if (args.Length > 1 || activationData != null)
+            var activationData = AppDomain.CurrentDomain.SetupInformation.ActivationArguments?.ActivationData;
+
+            if (args.Length > 1)
             {
-                LoadPattern(args.Length > 1 ? args[1] : activationData[0]);
+                LoadPattern(args[1]);
+            }
+            else if (activationData != null)
+            {
+                LoadPattern(activationData[0]);
             }
         }
 
